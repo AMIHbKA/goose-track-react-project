266c004ef20292bfb497a629f08edd0f8325e0af
AMIHbKA <bishop0013forum@gmail.com>
2023-08-25 04:07:05 +0300

    init2

---
+++ src/UI/GlobalStyles.js
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
+
+export const GlobalStyle = createGlobalStyle`
+html {
+
+  .no-scroll {
+    overflow-y: hidden;
+  }
+}
+
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+  color: #212121;
+  background-color: #fff;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+
+ul, li, p, h1, h2, h3, h4 ,h5, h6 {
+    margin: 0;
+    padding: 0;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import { GlobalStyle } from './UI';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyle />
     <BrowserRouter basename="/goose-track-react-project">
       <App />
     </BrowserRouter>
