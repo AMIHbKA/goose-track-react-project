325898de9c5cf6f920adf08ed5bba77663004049
Ivan Klimov <klimov128@gmail.com>
2023-08-26 20:55:55 +0300

    Update App.jsx
    Перенес кнопку в Layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,9 +13,12 @@ export const App = () => {
   return (
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
       <GlobalStyle />
-      <button onClick={switchTheme}>Switch Theme</button>
+      {/* <button onClick={switchTheme}>Switch Theme</button> */}
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route
+          path="/"
+          element={<Layout currentTheme={theme} switchTheme={switchTheme} />}
+        >
           <Route index element={<div>MainPage</div>} />
           <Route path="/login" element={<div>LoginPage</div>} />
           <Route path="/register" element={<div>RegisterPage</div>} />
