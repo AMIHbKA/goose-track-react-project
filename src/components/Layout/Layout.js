1466a8ddb4d8ebb53c12f23cef673ce315aefe69
Ivan Klimov <klimov128@gmail.com>
2023-08-26 23:52:00 +0300

    Update Layout.js
    добавил меню

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,9 +4,22 @@ import { Link, Outlet } from 'react-router-dom';
 import { MenuIcon, MoonIcon, SunIcon } from 'UI';
 import { Container, Modal } from 'components';
 import { Button, Header, IconsButton, UserInfo } from './Layout.styled';
+import { BurgerMenu } from 'components/BurgerMenu/BurgerMenu';
+import { CloseButton } from 'components/BurgerMenu/BurgerMenu.styled';
 
 export const Layout = ({ currentTheme, switchTheme }) => {
   const [showModal, setShowModal] = useState(false);
+  const [isBurgerMenuOpen, setBurgerMenuOpen] = useState(false);
+
+  const showBurgerMenu = () => {
+    console.log('click showBurgerMenu');
+    setBurgerMenuOpen(true);
+  };
+
+  const closeBurgerMenu = () => {
+    console.log(isBurgerMenuOpen);
+    setBurgerMenuOpen(false);
+  };
   const onShowModal = () => {
     setShowModal(s => !s);
   };
@@ -14,17 +27,48 @@ export const Layout = ({ currentTheme, switchTheme }) => {
   return (
     <>
       <Header>
-        {/* <div>
-          <div style={{ display: 'flex', gap: 20 }}>
-            <p>logo</p>
-            <p>GooseTrack</p>
-          </div>
-          <nav>
-            <Link to="/account">Account Page</Link>
-            <Link to="/calendar">Calendar Page</Link>
-            <Link to="/statistics">Statistics Page</Link>
-          </nav>
-        </div> */}
+        {isBurgerMenuOpen && (
+          <BurgerMenu onActive={closeBurgerMenu}>
+            <div
+              style={{
+                display: 'flex',
+                gap: 25,
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                marginBottom: 64,
+              }}
+            >
+              <div style={{ display: 'flex' }}>
+                <p>logo</p>
+                <p
+                  style={{
+                    fontSize: 16,
+                    fontWeight: 700,
+                    lineHeight: 1.375,
+                    color: '#3E85F3',
+                    textShadow:
+                      '0px 9.399999618530273px 57.6875px rgba(0, 0, 0, 0.04), 0px 47px 355px rgba(0, 0, 0, 0.07)',
+                  }}
+                >
+                  GooseTrack
+                </p>
+              </div>
+              <CloseButton size="24" type="button" onClick={closeBurgerMenu} />
+            </div>
+            <p style={{ marginBottom: 24 }}>User Panel</p>
+            <nav style={{ display: 'flex', flexDirection: 'column', gap: 18 }}>
+              <Link to="/account" onClick={closeBurgerMenu}>
+                Account Page
+              </Link>
+              <Link to="/calendar" onClick={closeBurgerMenu}>
+                Calendar Page
+              </Link>
+              <Link to="/statistics" onClick={closeBurgerMenu}>
+                Statistics Page
+              </Link>
+            </nav>
+          </BurgerMenu>
+        )}
         <div
           style={{
             display: 'flex',
@@ -34,7 +78,7 @@ export const Layout = ({ currentTheme, switchTheme }) => {
             outline: '2px solid purple',
           }}
         >
-          <IconsButton>
+          <IconsButton onClick={showBurgerMenu}>
             {
               <MenuIcon
                 size={24}
