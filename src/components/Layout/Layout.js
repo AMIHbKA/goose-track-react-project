fbb2dce5c29143aab53346ce3615fea435dfd545
AMIHbKA <95082331+AMIHbKA@users.noreply.github.com>
2023-08-31 13:41:45 +0300

    update layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,36 @@
-// import { Container } from 'components';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
-export const Layout = () => {
+// import { Container } from 'components';
+import { HeaderLayout } from 'components/HeaderLayout/HeaderLayout';
+import { useWindowWidth } from 'hooks/useWindowWidth';
+import { MenuPanel } from 'components/MenuPanel/MenuPanel';
+import {
+  GridContainer,
+  GridMainPanel,
+  GridMenuPanel,
+  GridUserPanel,
+  Main,
+} from './LayoutStyled';
+
+export const Layout = ({ currentTheme, switchTheme }) => {
+  const windowWidth = useWindowWidth();
+
   return (
-    <main>
-      {/* <Container outline> */}
-      <Suspense fallback={null}>
-        <Outlet />
-      </Suspense>
-      {/* </Container> */}
-    </main>
+    <GridContainer>
+      <GridMenuPanel> {windowWidth > 1024 && <MenuPanel />}</GridMenuPanel>
+      <GridUserPanel>
+        <HeaderLayout currentTheme={currentTheme} switchTheme={switchTheme} />
+      </GridUserPanel>
+      <GridMainPanel>
+        <Main>
+          {/* <Container outline> */}
+          <Suspense fallback={null}>
+            <Outlet />
+          </Suspense>
+          {/* </Container> */}
+        </Main>
+      </GridMainPanel>
+    </GridContainer>
   );
 };
