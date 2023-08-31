8372de2108dce57defc9db90e576a42598836ba8
AMIHbKA <bishop0013forum@gmail.com>
2023-08-31 07:32:44 +0300

    Update GlobalStyles.js

--- src/UI/GlobalStyles.js
+++ src/UI/GlobalStyles.js
@@ -6,75 +6,92 @@ import InterSemiBold from './fonts/Inter/Inter-SemiBold.woff2';
 import InterBold from './fonts/Inter/Inter-Bold.woff2';
 import PoppinsRegular from './fonts/Poppins/Poppins-Regular.woff2';
 import PoppinsMedium from './fonts/Poppins/Poppins-Medium.woff2';
+import CoolveticaRegular from './fonts/Coolvetica/Coolvetica-Regular.woff2';
+import CoolveticaItalic from './fonts/Coolvetica/Coolvetica-Italic.woff2';
 
 export const GlobalStyle = createGlobalStyle`
   @font-face {
-    font-family: 'Inter';
+    font-family: Inter, sans-serif;
     font-weight: 700;
     src: url(${InterBold}) format('woff2');
   font-display: fallback;
   }
 
   @font-face {
-    font-family: 'Inter';
+    font-family: Inter, sans-serif;
     font-weight: 600;
       src: url(${InterSemiBold}) format('woff2');
   font-display: fallback;
   }
 
   @font-face {
-    font-family: 'Inter';
+    font-family: Inter, sans-serif;
     font-weight: 500;
     src: url(${InterMedium}) format('woff2');
     font-display: fallback;
   }
 
   @font-face {
-    font-family: 'Inter';
-    src: url(${InterRegular}) format('woff2');
-    font-weight: 400;
-    font-display: fallback;
-  }
-
-  @font-face {
-    font-family: 'Poppins';
-    font-weight: 500;
-    src: url(${PoppinsMedium}) format('woff2');
-    font-display: fallback;
-  }
-
-  @font-face {
-    font-family: 'Poppins';
-    font-weight: 400;
-    src: url(${PoppinsRegular}) format('woff2');
-    font-display: fallback;
-
-  }
-
-  html {
     font-family: Inter, sans-serif;
-    font-weight: 500;
-    font-size: 14px;
+    font-weight: 400;
+    src: url(${InterRegular}) format('woff2');
+    font-display: fallback;
+  }
+
+@font-face {
+  font-family: Poppins, sans-serif;
+  font-weight: 500;
+  src: url(${PoppinsMedium}) format('woff2');
+  font-display: fallback;
+}
+
+@font-face {
+  font-family: Poppins, sans-serif;
+  font-weight: 400;
+  src: url(${PoppinsRegular}) format('woff2');
+  font-display: fallback;
+}
+
+@font-face {
+  font-family: Coolvetica, sans-serif;
+  font-weight: 400;
+  src: url(${CoolveticaRegular}) format('woff2');
+  font-display: fallback;
+}
+
+@font-face {
+  font-family: Coolvetica, sans-serif;
+  font-weight: 400;
+  font-style: italic;
+  src: url(${CoolveticaItalic}) format('woff2');
+  font-display: fallback;
+}
+
+html {
+  font-family: Inter, sans-serif;
+  font-weight: 500;
+  font-size: 14px;
     .no-scroll {
       overflow-y: hidden;
     }
   }
 
-  body {
-    margin: 0;
-    line-height: normal;
-    color: ${props => props.theme.colors.mainText};
-    background: ${props => props.theme.colors.mainBack};
-    transition: .3s ease;
-  }
+body {
+  margin: 0;
 
-  img {
-    display: block;
-    max-width: 100%;
-    height: auto;
-  }
+  line-height: normal;
+  color: ${props => props.theme.colors.mainText};
+  background: ${props => props.theme.colors.mainBack};
+  transition: .3s ease;
+}
 
-  ul, li, p, h1, h2, h3, h4 ,h5, h6 {
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+
+ul, ol, li, p, h1, h2, h3, h4 ,h5, h6 {
     margin: 0;
     padding: 0;
   }
@@ -95,7 +112,7 @@ export const GlobalStyle = createGlobalStyle`
     height: 100%;
   }
 
-  button {
+button {
     cursor: pointer;
   }
 
