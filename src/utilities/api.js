ad6c4fd13650837de872c0dee107734aa607a64f
AMIHbKA <bishop0013forum@gmail.com>
2023-09-02 00:40:25 +0300

    api added

---
+++ src/utilities/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://goose-track-backend-odyh.onrender.com/',
+});
+
+const setAuthHeader = token => {
+  instance.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+// Remove JWT
+const clearAuthHeader = () => {
+  instance.defaults.headers.common.Authorization = '';
+};
+
+export const api = {
+  instance,
+  setAuthHeader,
+  clearAuthHeader,
+};
