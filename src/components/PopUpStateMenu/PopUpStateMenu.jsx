4b3fa1c4ba9a16590ef9165ef51a007b41e04e34
AMIHbKA <95082331+AMIHbKA@users.noreply.github.com>
2023-09-07 17:16:36 +0300

    mui error

--- src/components/PopUpStateMenu/PopUpStateMenu.jsx
+++ src/components/PopUpStateMenu/PopUpStateMenu.jsx
@@ -1,36 +1,36 @@
-import { Menu, MenuItem } from '@material-ui/core';
-import { useState } from 'react';
+// import { Menu, MenuItem } from '@material-ui/core';
+// import { useState } from 'react';
 
-export const PopUpStateMenu = () => {
-  const [anchorEl, setAnchorEl] = useState(null);
+// export const PopUpStateMenu = () => {
+//   const [anchorEl, setAnchorEl] = useState(null);
 
-  const handleClick = event => {
-    setAnchorEl(event.currentTarget);
-  };
+//   const handleClick = event => {
+//     setAnchorEl(event.currentTarget);
+//   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+//   const handleClose = () => {
+//     setAnchorEl(null);
+//   };
 
-  return (
-    <div>
-      <button
-        aria-controls="simple-menu"
-        aria-haspopup="true"
-        onClick={handleClick}
-      >
-        Open Menu
-      </button>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose}>In Progress</MenuItem>
-        <MenuItem onClick={handleClose}>Done</MenuItem>
-      </Menu>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <button
+//         aria-controls="simple-menu"
+//         aria-haspopup="true"
+//         onClick={handleClick}
+//       >
+//         Open Menu
+//       </button>
+//       <Menu
+//         id="simple-menu"
+//         anchorEl={anchorEl}
+//         keepMounted
+//         open={Boolean(anchorEl)}
+//         onClose={handleClose}
+//       >
+//         <MenuItem onClick={handleClose}>In Progress</MenuItem>
+//         <MenuItem onClick={handleClose}>Done</MenuItem>
+//       </Menu>
+//     </div>
+//   );
+// };
